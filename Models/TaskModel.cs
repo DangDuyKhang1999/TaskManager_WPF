4c8ea6e1454ae9f5599b9618185cdeeb5dca0075
DangDuyKhang1999 <DangDuyKhang7999@gmail.com>
2025-05-11 21:10:58 +0700

    [Khang][Add] Create simple screens with hardcoded values

---
+++ Models/TaskModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace TaskManager.Models
+{
+    public class TaskModel : INotifyPropertyChanged
+    {
+        private string _assignee;
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+
+        public string Assignee
+        {
+            get => _assignee;
+            set
+            {
+                if (_assignee != value)
+                {
+                    _assignee = value;
+                    OnPropertyChanged(nameof(Assignee));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using TaskManager.Models;
+
+namespace TaskManager.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<TaskModel> Tasks { get; set; }
+        public ObservableCollection<string> AvailableAssignees { get; set; }
+
+        public MainViewModel()
+        {
+            AvailableAssignees = new ObservableCollection<string>
+            {
+                "Nguyễn Văn A", "Trần Thị B", "Lê Văn C", "Phạm Văn D"
+            };
+
+            Tasks = new ObservableCollection<TaskModel>
+            {
+                new TaskModel { Id = "CV001", Title = "Viết tài liệu", Description = "Soạn tài liệu sử dụng", Assignee = "Nguyễn Văn A", Status = "Chưa thực hiện" },
+                new TaskModel { Id = "CV002", Title = "Kiểm thử", Description = "Test module đăng nhập", Assignee = "Trần Thị B", Status = "Đang thực hiện" },
+                new TaskModel { Id = "CV003", Title = "Triển khai", Description = "Cài đặt server", Assignee = "Lê Văn C", Status = "Hoàn thành" }
+            };
+        }
+    }
+}
