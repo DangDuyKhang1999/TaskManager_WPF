9385b339c2741b24d2ea24c0360ed2a2aa66616a
DangDuyKhang1999 <DangDuyKhang7999@gmail.com>
2025-05-15 22:13:14 +0700

    [Khang][Update] Update project structure

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using TaskManager.Views;
 
 namespace TaskManager
 {
--- Views/LoginWindow.xaml.cs
+++ Views/LoginWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data.SqlClient;
 using System.Windows;
 
-namespace TaskManager
+namespace TaskManager.Views
 {
     public partial class LoginWindow : Window
     {
