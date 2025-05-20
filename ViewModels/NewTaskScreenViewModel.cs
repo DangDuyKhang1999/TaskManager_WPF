33a7535d870829196a22cb3fd766768859fc8064
DangDuyKhang1999 <DangDuyKhang7999@gmail.com>
2025-05-20 22:11:52 +0700

    [Khang][Add] Get data from task creation form

--- ViewModels/NewTaskScreenViewModel.cs
+++ ViewModels/NewTaskScreenViewModel.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Windows.Input;
 using TaskManager.Common;
 using TaskManager.Contexts;
 using TaskManager.Data;
+using TaskManager.Models;
+using TaskManager.Services;
 
 namespace TaskManager.ViewModels
 {
@@ -26,6 +31,51 @@ namespace TaskManager.ViewModels
             set => SetProperty(ref _assigneeId, value);
         }
 
+        private string _code;
+        public string Code
+        {
+            get => _code;
+            set => SetProperty(ref _code, value);
+        }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        private int _status;
+        public int Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        private DateTime? _dueDate;
+        public DateTime? DueDate
+        {
+            get => _dueDate;
+            set => SetProperty(ref _dueDate, value);
+        }
+
+        private int _priority;
+        public int Priority
+        {
+            get => _priority;
+            set => SetProperty(ref _priority, value);
+        }
+
+        public ICommand SaveCommand { get; }
+        public ICommand CancelCommand { get; }
+
         public NewTaskScreenViewModel()
         {
             _userRepository = new UserRepository(AppConstants.Database.ConnectionString);
@@ -36,6 +86,62 @@ namespace TaskManager.ViewModels
 
             ReporterUsers = new ObservableCollection<string>(DatabaseContext.Instance.AdminUsersList);
             AssigneeUsers = new ObservableCollection<string>(DatabaseContext.Instance.NormalUsersList);
+
+            // Khởi tạo command
+            SaveCommand = new RelayCommand(_ => SaveTask());
+            CancelCommand = new RelayCommand(_ => Cancel());
+        }
+
+        private void SaveTask()
+        {
+            var task = new TaskModel
+            {
+                Code = Code,
+                Title = Title,
+                Description = Description,
+                Status = Status,
+                Priority = Priority,
+                DueDate = DueDate ?? DateTime.Now,
+                ReporterId = ReporterId,
+                AssigneeId = AssigneeId,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            var reporterCode = GetEmployeeCodeByDisplayName(task.ReporterId);
+            var assigneeCode = GetEmployeeCodeByDisplayName(task.AssigneeId);
+
+            Logger.Instance.Info(
+                $"[New Task Created]\n" +
+                $"- Code: {task.Code}\n" +
+                $"- Title: {task.Title}\n" +
+                $"- Description: {task.Description}\n" +
+                $"- Status: {task.StatusString}\n" +
+                $"- Priority: {task.PriorityString}\n" +
+                $"- Reporter (EmployeeCode): {reporterCode ?? "Unknown"}\n" +
+                $"- Assignee (EmployeeCode): {assigneeCode ?? "Unknown"}\n" +
+                $"- DueDate: {task.DueDate:yyyy-MM-dd}\n" +
+                $"- CreatedAt: {task.CreatedAt:yyyy-MM-dd HH:mm:ss}"
+            );
+
+            // TODO: Thêm logic lưu task vào database nếu cần
+        }
+
+        private void Cancel()
+        {
+            // TODO: Xử lý hủy, ví dụ reset dữ liệu hoặc đóng màn hình
+        }
+
+        private string? GetEmployeeCodeByDisplayName(string displayName)
+        {
+            using var connection = new SqlConnection(AppConstants.Database.ConnectionString);
+            connection.Open();
+
+            using var command = new SqlCommand("SELECT EmployeeCode FROM Users WHERE DisplayName = @displayName", connection);
+            command.Parameters.AddWithValue("@displayName", displayName);
+
+            var result = command.ExecuteScalar();
+            return result?.ToString();
         }
     }
 }
