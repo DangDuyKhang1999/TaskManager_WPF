e7a4d80fe04274bdb2ce3962b33e95a227976cb9
DangDuyKhang1999 <DangDuyKhang7999@gmail.com>
2025-05-24 14:51:48 +0700

    [Khang][Update] Update follow MVVM

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,11 +7,18 @@ namespace TaskManager.ViewModels
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        // Tiêu đề của cửa sổ
         public string WindowTitle => AppConstants.AppText.MainWindowTitle;
 
+        // Trạng thái hiển thị tab New User
         public Visibility NewUserTabVisibility =>
-            UserSession.Instance.IsAdmin ? Visibility.Visible : Visibility.Collapsed;
+            UserSession.Instance?.IsAdmin == true ? Visibility.Visible : Visibility.Collapsed;
 
+        // Trạng thái hiển thị tab Users
+        public Visibility UsersTabVisibility =>
+            UserSession.Instance?.IsAdmin == true ? Visibility.Visible : Visibility.Collapsed;
+
+        // Xử lý sự kiện khi cửa sổ đóng
         public void OnWindowClosed()
         {
             Logger.Instance.Information(AppConstants.Logging.TaskManagerEnd);
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System.Windows;
-using TaskManager.Common;
-using TaskManager.Contexts;
-using TaskManager.Services;
-using TaskManager.ViewModels;
 
 namespace TaskManager.Views
 {
@@ -11,28 +7,6 @@ namespace TaskManager.Views
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new TaskScreenViewModel();
-            // Show the "New User" tab only if the current user is an admin
-            if (UserSession.Instance.IsAdmin)
-            {
-                NewUserTab.Visibility = Visibility.Visible;
-                UsersTab.Visibility = Visibility.Visible;
-            }
-
-            // Center the window on the screen
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-
-            // Register event handler for window close event
-            this.Closed += MainWindow_Closed;
-        }
-
-        /// <summary>
-        /// Handler for the window Closed event
-        /// Logs the application shutdown info
-        /// </summary>
-        private void MainWindow_Closed(object? sender, System.EventArgs e)
-        {
-            Logger.Instance.Information(AppConstants.Logging.TaskManagerEnd);
         }
     }
 }
